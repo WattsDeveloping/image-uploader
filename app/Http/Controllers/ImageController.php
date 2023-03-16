e1999ac9c02979653f5db6a95a60060328d48bf9
Michael Watts <wattsdeveloping@gmail.com>
2023-03-16 17:13:04 +0000

    Initial Commit of repo
    This is my first integration with the FilePond dependency

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ImageController extends Controller
+{
+    public function index()
+    {
+        return view('images.index');
+    }
+
+    public function show()
+    {
+    }
+
+    public function upload()
+    {
+    }
+}
---
+++ resources/js/components/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="mt-4">
+        <file-pond name="image" ref="pond" label-idle="Click to chose image, or drag here..." server="/upload"
+            @init="filepondInitalised" />
+    </div>
+</template>
+<script>
+import vueFilePond from 'vue-filepond';
+import "filepond/dist/filepond.min.css";
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+
+const FilePond = vueFilePond(FilePondPluginFileValidateType);
+
+export default {
+    components: {
+        FilePond
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        filepondInitialised() {
+
+        }
+    }
+}
+</script>
